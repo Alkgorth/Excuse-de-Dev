58b8a564468174afbf1514e50c57ec848306d104
Alkgorth <ap.percheron@gmail.com>
2025-07-18 17:17:03 +0200

    Création arborescence

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 namespace App\Controller;
 
 class Controller
@@ -10,7 +10,7 @@ class Controller
             if (isset($_GET['controller'])) {
                 switch ($_GET['controller']) {
                     case 'pages':
-                        $pageController = new UserController();
+                        $pageController = new HomeController();
                         $pageController->route();
                         break;
                     default:
@@ -19,7 +19,7 @@ class Controller
                 }
             } else {
 
-                $pageController = new UserController();
+                $pageController = new HomeController();
                 $pageController->home();
             }
         } catch (\Exception $e) {
--- index.php
+++ index.php
@@ -1,25 +1,10 @@
 <?php
 
 echo "Bienvenue sur l'application Excuses de Dev !";
-/*
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-session_start();
-
-if (!isset($_COOKIE['csrfToken'])) {
-    $token = bin2hex(random_bytes(32));
-    setcookie(
-        'csrfToken',
-        $token,
-        time() + 3600,
-        '/',
-        '',
-        false,
-        true
-    );
-}
 
 define('_ROOTPATH_', __DIR__);
 define('_TEMPLATEPATH_', __DIR__.'/templates');
@@ -29,6 +14,5 @@ use App\Controller\Controller;
 
 $controller = new Controller;
 $controller->route();
-*/
 
 ?>
