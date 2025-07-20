d0ec4cca40424a37e5eea7c5a4582defe46e8fc4
Alkgorth <ap.percheron@gmail.com>
2025-07-20 13:28:03 +0200

    Ajout fonction JS pageLostRedirection

--- templates/pages/lost.php
+++ templates/pages/lost.php
@@ -1,5 +1,27 @@
+<?php
+
+    require_once _ROOTPATH_ . '/templates/head.php';
+
+?>
+<body id="lost-page">
 <main>
     <section>
         <h1>Page "You'r lost !"</h1>
     </section>
+
+    <section class="row d-flex container justify-content-center">
+
+        <div class=" container-fluid text-center">
+        <h1>i'm lost 😢</h1>
+        <div class="gif-container">
+            <img src="https://media.giphy.com/media/efJ3wWdInLzSS/giphy.gif" alt="GIF I'm Lost" class="img-fluid">
+        </div>
+    </div>
+    </section>
 </main>
+
+<?php
+
+    require_once _ROOTPATH_ . '/templates/footer.php';
+
+?>
