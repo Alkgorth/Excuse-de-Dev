deb387229a382dd3ac95d52945498da5f51d53ed
Alkgorth <ap.percheron@gmail.com>
2025-07-20 23:43:02 +0200

    Modification de script.js

--- App/Controller/ApiExcusesController.php
+++ App/Controller/ApiExcusesController.php
@@ -14,8 +14,8 @@ class ApiExcusesController extends Controller
             //On met en place une condition pour lancer le bon controller
             if (isset($_GET['action'])) {
                 switch ($_GET['action']) {
-                    case 'apiExcuses':
-                        $this->apiExcuses();
+                    case 'getExcuses':
+                        $this->getExcuses();
                         break;
                      default:
                         throw new \Exception("Cette action n'existe pas : " . $_GET['action']);
@@ -31,7 +31,24 @@ class ApiExcusesController extends Controller
         }
     }
 
-    public function apiExcuses() {
-        
+    public function getExcuses() {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $data = file_get_contents(_ROOTPATH_ . '/Assets/data/excuses.json');
+            echo $data;
+            exit;
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $json = file_get_contents('php://input');
+            $newExcuse =json_decode($json, true);
+
+            $excuses = json_decode(file_get_contents(_ROOTPATH_ . '/Assets/data/excuses.json'), true);
+            $excuses = $newExcuse;
+
+            file_put_contents('./Assets/data/excuses.json', json_encode($excuses, JSON_PRETTY_PRINT));
+            echo json_encode(['status' => 'ok']);
+            exit;
+        }
     }
 }
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-
+// Redirection de la page lost.php
 function pageLostRedirection() {
     if (document.body.id === 'lost-page') {
         
@@ -9,6 +9,28 @@ function pageLostRedirection() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    pageLostRedirection();
+    pageLostRedirection();  
     
-})
+    // Affichage des excuses sur la page home.php
+    const excuseButton = document.getElementById("excuses");
+    const inputExcuse = document.getElementById("inputExcuse");
+    
+    excuseButton.addEventListener("click", async() => {
+        try {
+            const response = await fetch("index.php?controller=apiExcuses&action=getExcuses");
+            const data = await response.json();
+    
+            if (data.excuse) {
+                inputExcuse.value = data.excuse;
+            } else {
+                inputExcuse.value = "Erreur : aucune excuse trouvée";
+            }
+    
+        } catch (error) {
+            inputExcuse.value = "Erreur réseau ou serveur";
+            console.error("Erreur : ", error);
+        }
+    
+    });
+});
+
